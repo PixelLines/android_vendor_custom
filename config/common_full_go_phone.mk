c64b86c78ae62e3b703dea8cc056f814a88c1586
kentaro aldanatien ramadhan <nkbob2707@gmail.com>
2026-02-02 21:00:25 +0700

    Update common_full_go_phone.mk

--- config/common_full_go_phone.mk
+++ config/common_full_go_phone.mk
@@ -1,5 +1,5 @@
 # Inherit some common lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_go_phone.mk)
 
-# Inherit some common PixelOS stuff.
+# Inherit some common PixelLines stuff.
 $(call inherit-product, vendor/custom/config/common.mk)
