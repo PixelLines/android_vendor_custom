d1fed581b059cef6981a6622928d8b6dbade3e78
kentaro aldanatien ramadhan <nkbob2707@gmail.com>
2026-02-02 21:07:23 +0700

    Update version.mk

--- config/version.mk
+++ config/version.mk
@@ -5,16 +5,16 @@ CUSTOM_PLATFORM_VERSION := 16.0
 CUSTOM_VERSION := $(CUSTOM_BUILD)-$(CUSTOM_PLATFORM_VERSION)-$(CUSTOM_BUILD_DATE)
 CUSTOM_VERSION_PROP := sixteen
 
-# PixelOS Platform Version
+# PixelLines Platform Version
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.custom.build.date=$(CUSTOM_BUILD_DATE) \
     ro.custom.device=$(CUSTOM_BUILD) \
     ro.custom.fingerprint=$(ROM_FINGERPRINT) \
-    ro.custom.version=PixelOS_$(CUSTOM_VERSION)
+    ro.custom.version=PixelLines$(CUSTOM_VERSION)
 
 # Updater
 ifeq ($(IS_OFFICIAL),true)
     PRODUCT_PRODUCT_PROPERTIES += \
-        net.pixelos.build_type=ci \
-        net.pixelos.version=$(CUSTOM_VERSION_PROP)
+        net.pixellines.build_type=ci \
+        net.pixellines.version=$(CUSTOM_VERSION_PROP)
 endif
